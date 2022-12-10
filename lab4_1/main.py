e1f86f9d1b6d788c54c716cf765fb128d3c698dc
rakhmukova <lessness19@gmail.com>
2022-12-10 18:33:21 +0300

    Complete 4.1 lab

---
+++ lab4_1/main.py
@@ -0,0 +1,35 @@
+import math
+
+from common.main import print_lab
+from lab4_1.integrate import Integration
+
+if __name__ == '__main__':
+    a = 0
+    b = 1
+
+    def f(x):
+        return math.sin(x)
+
+    def p(x): return 1 / math.sqrt(1 - x)
+
+    x1 = 1 / 6
+    x2 = 1 / 2
+    x3 = 5 / 6
+    nodes = [x1, x2, x3]
+    # m = 10
+    # h = (b - a) / m
+    # nodes = [a + i * h for i in range(m)]
+
+    print_lab(4.1, "Точное и приближённое вычисление интеграла по квадратурным формулам")
+    print(f"Вариант 8. \nf(x)=sin(x) \np(x) = 1/sqrt(1 - x) \na={a}  b={b}")
+
+    integration = Integration(a, b, f, p)
+
+    precise_value = integration.precise()
+    approximate_value = integration.approximate(nodes)
+
+    print(f"\nПриближенное значение интеграла: {approximate_value}")
+    print(f"Точное значение интеграла: {precise_value}")
+    abs_mistake = abs(precise_value - float(approximate_value))
+    print(f"Значение абсолютной погрешности: {abs_mistake}")
+    print(f"Значение относительной погрешности: {abs(abs_mistake/precise_value)}")
