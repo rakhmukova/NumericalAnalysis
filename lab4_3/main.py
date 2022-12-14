8202ee08aeb55b8feca8dd7b75ce9e6b5d95c11b
rakhmukova <lessness19@gmail.com>
2022-12-14 22:43:03 +0300

    [4.3] Introduce execution loop

--- lab4_3/main.py
+++ lab4_3/main.py
@@ -3,17 +3,15 @@ from sympy import symbols, lambdify, exp
 from common.main import print_lab, tabulate_results
 from lab4_1.integrate import Integration
 
-if __name__ == '__main__':
-    print_lab(4.3, 'Приближённое вычисление интеграла по составным квадратурным формулам')
-    print(f"Вариант 8.\n")
 
+def execute():
     x = symbols('x')
     exp_func = exp(x)
     zero_func = 0
     poly_0_func = 1
     poly_1_func = x
-    poly_2_func = x**2
-    poly_3_func = x**3
+    poly_2_func = x ** 2
+    poly_3_func = x ** 3
     functions = [exp_func, poly_0_func, poly_1_func, poly_2_func, poly_3_func]
     derivatives = [
         [exp_func, exp_func, exp_func, exp_func],
@@ -26,11 +24,11 @@ if __name__ == '__main__':
     num_of_function = int(input('\nВведите номер функции (0): ') or '0')
 
     f = lambdify(x, functions[num_of_function])
-    ders = list(map(lambda der : lambdify(x, der), derivatives[num_of_function]))
+    ders = list(map(lambda der: lambdify(x, der), derivatives[num_of_function]))
 
     a = float(input("Введите левый предел интегрирования (0.0): ") or '0')
     b = float(input("Введите правый предел интегрирования (1.0): ") or '1')
-    m = int(input("Введите число промежутков деления (10): ") or '10')
+    m = int(input("Введите число промежутков деления (100): ") or '100')
 
     def p(x): return 1
 
@@ -56,3 +54,13 @@ if __name__ == '__main__':
         print(f"Абсолютная погрешность: {absolute_error}")
         print(f'Относительная погрешность: {absolute_error / abs(precise_value)}')
         print(f'Теоретическая погрешность: {theoretical_error}\n')
+
+
+if __name__ == '__main__':
+    print_lab(4.3, 'Приближённое вычисление интеграла по составным квадратурным формулам')
+    print(f"Вариант 8.\n")
+
+    to_quit = 1
+    while to_quit != 0:
+        execute()
+        to_quit = int(input("\nВведите 0, чтобы закрыть программу, другую цифру, чтобы продолжить: "))
