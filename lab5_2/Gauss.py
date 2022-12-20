a0fb03e4e8e64deed236f6e714e4cc0e4802c31a
rakhmukova <lessness19@gmail.com>
2022-12-20 10:51:07 +0300

    [5.2] Refactor Gauss

--- lab5_2/Gauss.py
+++ lab5_2/Gauss.py
@@ -3,7 +3,7 @@ import math
 
 from sympy import symbols, simplify, lambdify
 
-from common.functions import print_lab, tabulate_results
+from common.functions import print_lab, tabulate_results, abs_error
 from lab1.main import Solver
 from common.integrate import Integration
 
@@ -36,10 +36,9 @@ class GaussIntegration:
 
     def calc_coefficients(self, polynomials, nodes):
         coefficients = []
-        for i in range(self.N):  # self.N
+        for i in range(self.N):
             node = nodes[i]
             polynom = polynomials[i]
-            # print(f'node {node[0]} poly {polynom(node[0])}')
             current_coefficients = [2*(1 - node[j]**2) / (((i + 1) * polynom(node[j]))**2) for j in range(i + 1)]
             coefficients.append(current_coefficients)
 
@@ -73,33 +72,51 @@ class GaussIntegration:
         a, b = list(map(lambda x: float(x), borders))
         return a, b
 
-    def integrate_for_custom_borders(self, a, b, degree, polynomials_nodes, coefficients):
+    def integrate_for_custom_borders(self, degree, polynomials_nodes, coefficients, f=None, a=-1, b=1):
         custom_nodes = copy.deepcopy(polynomials_nodes[degree - 1])
         custom_coefficients = copy.deepcopy(coefficients[degree - 1])
-        # tabulate_results(zip(custom_nodes, custom_coefficients), headers=['узел', 'коэфф'],
-        #                  title=f'Узлы и коэффициенты КФ Гаусса для N={degree}')
         sum_int = 0
         half = (b - a) / 2
         middle = (b + a) / 2
+        if f is None:
+            f = self.f
         for i in range(degree):
             custom_coefficients[i] *= half
             custom_nodes[i] = half * custom_nodes[i] + middle
-            sum_int += custom_coefficients[i] * self.f(custom_nodes[i])
-        tabulate_results(zip(custom_nodes, custom_coefficients), headers=['узел', 'коэфф'],
-                         title=f'Узлы и коэффициенты КФ Гаусса для N={degree}')
+            sum_int += custom_coefficients[i] * f(custom_nodes[i])
+        if [a, b] != [-1, 1]:
+            tabulate_results(zip(custom_nodes, custom_coefficients), headers=['узел', 'коэфф'],
+                             title=f'Узлы и коэффициенты КФ Гаусса для N={degree}')
         return sum_int
 
     def integrate_custom(self, polynomials_nodes, coefficients):
         a, b = self.input_borders()
         for degree in self.degrees:
-            approximate_value = self.integrate_for_custom_borders(a, b, degree, polynomials_nodes, coefficients)
+
+            approximate_value = self.integrate_for_custom_borders(degree, polynomials_nodes, coefficients, a=a, b=b)
             integration = Integration(a, b, self.f, lambda x: 1)
             precise_value = integration.precise()
             print(f'\nПриближенное значение интеграла: {approximate_value}')
             print(f"Точное значение интеграла: {precise_value}")
 
+    def check_for_polynomials(self, polynomials_nodes, coefficients):
+        eps = math.pow(10, -12)
+        print(f'\nПроверим точность для многочленов (eps = {eps}):')
+        for degree in self.degrees:
+            def polynom(x): return 2 * x ** (2 * degree - 1)
+            integration = Integration(-1, 1, polynom, lambda x: 1)
+            precise_value = integration.precise()
+            approximate_value = self.integrate_for_custom_borders(degree, polynomials_nodes, coefficients, polynom)
+            print(f'\nПриближенное значение интеграла: {approximate_value}')
+            print(f"Точное значение интеграла: {precise_value}")
+            if abs_error(precise_value, approximate_value) < eps:
+                print('Проверка пройдена')
+            else:
+                print('Проверка не пройдена')
+
     def integrate(self):
         polynomials_nodes, coefficients = self.find_common_nodes_and_coefficients()
+        self.check_for_polynomials(polynomials_nodes, coefficients)
         to_quit = 1
         while to_quit != 0:
             self.integrate_custom(polynomials_nodes, coefficients)
@@ -120,7 +137,6 @@ if __name__ == '__main__':
 
     integration = Integration(-1, 1, f, p)
     precise_value = integration.precise()
-    print(f"Точное значение интеграла: {precise_value}")
 
     degrees = [3, 6, 7, 8]
     gauss = GaussIntegration(f, degrees)
