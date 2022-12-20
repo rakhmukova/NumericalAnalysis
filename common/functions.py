03cbf114329b71facbd68bb57cdb3f87d3504fc2
rakhmukova <lessness19@gmail.com>
2022-12-20 19:43:08 +0300

    [common] Change output of execution_loop

--- common/functions.py
+++ common/functions.py
@@ -27,7 +27,7 @@ def execution_loop(execute, *args):
     to_quit = 1
     while to_quit != 0:
         execute(*args)
-        to_quit = int(input("\nВведите 0, чтобы закрыть программу, другую цифру, чтобы продолжить: "))
+        to_quit = int(input("\nВведите 0, чтобы закрыть программу, другую цифру для ввода новых параметров: "))
 
 
 def input_borders(def_a=None, def_b=None):
