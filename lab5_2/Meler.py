339f311c0368d9d63211fb47557d4ddd88c522d7
rakhmukova <lessness19@gmail.com>
2022-12-20 10:22:18 +0300

    [5.2] Refactor Meler

--- lab5_2/Meler.py
+++ lab5_2/Meler.py
@@ -1,6 +1,6 @@
 import math
 
-from common.functions import tabulate_results, abs_error
+from common.functions import tabulate_results, abs_error, print_lab
 from common.integrate import Integration
 
 
@@ -24,6 +24,8 @@ class MelerIntegration:
 
 
 if __name__ == '__main__':
+    print_lab(5.2, "Вычисление интегралов при помощи КФ Мелера")
+    print(f"Вариант 8. \nf(x)=exp(x) * sin(x^2)\n")
     def f(x):
         return math.exp(x) * math.sin(x ** 2)
     integartion = Integration(-1, 1, f, lambda x: 1 / math.sqrt(1 - x ** 2))
@@ -36,5 +38,7 @@ if __name__ == '__main__':
         approximate_values.append(value)
 
     print(f'\nТочное значение: {precise_value}')
+    print('\nПриближенные значения интеграла: ')
     absolute_errors = [abs_error(precise_value, approximate_value) for approximate_value in approximate_values]
-    tabulate_results(zip(degrees, approximate_values, absolute_errors), title='Приближенные значения интеграла')
+    for i in range(len(degrees)):
+        print(f'\nN = {degrees[i]}\nзначение: {approximate_values[i]}\nпогрешность: {absolute_errors[i]}')
