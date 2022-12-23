bec4ed72e543c63eb601af8cbfd9d328c99c106e
rakhmukova <lessness19@gmail.com>
2022-12-23 23:56:47 +0300

    [precise] Add constructor

--- integration_formula/precise.py
+++ integration_formula/precise.py
@@ -6,5 +6,8 @@ from integration_formula.integration import Integration
 
 
 class Precise(Integration):
+    def __init__(self, a, b, f, p):
+        super().__init__(a, b, f, p)
+
     def integrate(self):
         return integrate.quad(self.phi, self.a, self.b, epsabs=math.pow(10, -13), epsrel=math.pow(10, -13))[0]
