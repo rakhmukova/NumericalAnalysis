c9a7de256404e67b6b21751a097215f9a9e6d0ee
rakhmukova <lessness19@gmail.com>
2022-12-23 16:01:51 +0300

    [5.1] Add execuion loop

--- lab5_1/main.py
+++ lab5_1/main.py
@@ -1,14 +1,11 @@
 import math
 
-from common.functions import print_lab, input_borders, show_error_info
+from common.functions import print_lab, input_borders, show_error_info, execution_loop
 from integration_formula.gaussian import GaussianFormula
 from integration_formula.precise import Precise
 
 
-if __name__ == '__main__':
-    print_lab(5.1, "Приближённое вычисление интегралов при помощи КФ НАСТ")
-    print(f"Вариант 8. f(x)=sin(x) p(x)=exp(x)\n")
-
+def execute():
     a, b = input_borders(0, 1)
     N = int(input("Введите количество узлов (10): ") or '10')
 
@@ -17,7 +14,6 @@ if __name__ == '__main__':
 
     def p(x): return math.exp(x)
 
-
     precise = Precise(a, b, f, p)
     precise_value = precise.integrate()
 
@@ -25,3 +21,10 @@ if __name__ == '__main__':
     approximate_value = gaussian.integrate()
 
     show_error_info(precise_value, approximate_value)
+
+
+if __name__ == '__main__':
+    print_lab(5.1, "Приближённое вычисление интегралов при помощи КФ НАСТ")
+    print(f"Вариант 8. f(x)=sin(x) p(x)=exp(x)\n")
+
+    execution_loop(execute)
